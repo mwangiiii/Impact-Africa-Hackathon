821c7381c2f7c87bde72ab398e75c0ebe7d59875
Caldwell10 <caldwell.kibe@strathmore.edu>
2025-04-26 10:07:38 +0300

    Added Microsoft Speech Recognition

---
+++ mic_test.py
@@ -0,0 +1,16 @@
+import speech_recognition as sr
+recognizer = sr.Recognizer()
+
+with sr.Microphone() as source:
+    print("Adjusting for background noise...Please wait.")
+    recognizer.adjust_for_ambient_noise(source)
+    print("Listening...Speaking now!")
+
+    try:
+        audio = recognizer.listen(source)
+        text = recognizer.recognize_google(audio)
+        print("You said" + text)
+    except sr.UnknownValueError:
+        print("Sorry, could not understand audio")
+    except sr.RequestError as e:
+        print("API request failed")
